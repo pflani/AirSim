37a885de38b1d59634105db74b5ffa783c979bef
pflani <78821916+pflani@users.noreply.github.com>
2021-09-22 17:21:33 -0400

    added range and bearing to each point

--- PythonClient/multirotor/sensorframe_lidar_pointcloud.py
+++ PythonClient/multirotor/sensorframe_lidar_pointcloud.py
@@ -52,13 +52,15 @@ import setup_path
 import airsim
 import numpy as np
 import time
+import math
+from scipy.spatial.transform import Rotation as R
 
 class LidarTest:
 
     def __init__(self):
 
         # connect to the AirSim simulator
-        self.client = airsim.VehicleClient()
+        self.client = airsim.MultirotorClient() #changed from "Vehicle" to "Multirotor"
         self.client.confirmConnection()
         print('Connected!\n')
 
@@ -77,14 +79,16 @@ class LidarTest:
                     else:
                         f = open(filename,'a')
                     lidar_data = self.client.getLidarData(lidar_name=lidar_name,vehicle_name=vehicle_name)
+                    state = self.client.getMultirotorState(vehicle_name=vehicle_name) #is "vehicle_name" the only input?
                     
                     orientation = lidar_data.pose.orientation
-                    q0, q1, q2, q3 = orientation.w_val, orientation.x_val, orientation.y_val, orientation.z_val
+                    q0, q1, q2, q3 = orientation.w_val, orientation.x_val, orientation.y_val, orientation.z_val #quaternions for lidar
                     rotation_matrix = np.array(([1-2*(q2*q2+q3*q3),2*(q1*q2-q3*q0),2*(q1*q3+q2*q0)],
                                                   [2*(q1*q2+q3*q0),1-2*(q1*q1+q3*q3),2*(q2*q3-q1*q0)],
                                                   [2*(q1*q3-q2*q0),2*(q2*q3+q1*q0),1-2*(q1*q1+q2*q2)]))
 
                     position = lidar_data.pose.position
+                    multirotor_orientation = state.kinematics_estimated.orientation #another option: "state.rc_data.pitch", etc
                     for i in range(0, len(lidar_data.point_cloud), 3):
                         xyz = lidar_data.point_cloud[i:i+3]
 
@@ -93,9 +97,19 @@ class LidarTest:
                         final_y = corrected_y + position.y_val
                         final_z = corrected_z + position.z_val
 
+                        point_range = math.sqrt((position.x_val - final_x)**2 + (position.y_val - final_y)**2 + (position.z_val - final_z)**2)
+                        #angle to get from quadrotor orientation ("m..") to lidar orientation ("q..")
+                        m0, m1, m2, m3 = multirotor_orientation.w_val, multirotor_orientation.x_val, multirotor_orientation.y_val, multirotor_orientation.z_val
+                        m = R.from_quat([m0, -m1, -m2, -m3]) #manually make inverse/conjugate quaternion 
+                        q = R.from_quat([q0, q1, q2, q3])
+                        m_norm = m.as_quat()
+                        q_norm = q.as_quat()
+                        inner_product = (m_norm[0]*q_norm[0]) + (m_norm[1]*q_norm[1]) + (m_norm[2]*q_norm[2]) + (m_norm[3]*q_norm[3])
+                        point_angle = math.acos(2*(inner_product**2) - 1)
+
                         milli_time = round(time.time() * 1000)
 
-                        f.write("%f %f %f %f %d %d %d \n" % (final_x,final_y,final_z,milli_time,255,255,0))
+                        f.write("%f %f %f %f %f %f %d %d %d \n" % (final_x,final_y,final_z,point_range,point_angle,milli_time,255,255,0))
                     f.close()
                 existing_data_cleared = True
         except KeyboardInterrupt:
